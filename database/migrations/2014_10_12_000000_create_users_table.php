7cd9d4fec51ae19620e1d4668176583cae5a3370
Marlon Danilo Rocha Cardoso <marlondanrc@gmail.com>
2021-02-26 12:19:09 -0300

    Criada função para adicionar o usuário admin ao criar a tabela Users no banco de dados através da migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,11 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+use Ramsey\Uuid\Uuid;
+
 
 class CreateUsersTable extends Migration
 {
@@ -19,10 +24,24 @@ class CreateUsersTable extends Migration
             $table->string('nome');
             $table->string('cpf')->unique();
             $table->string('email')->unique();
+            $table->string('telefone')->nullable();
             $table->string('password');
+            $table->string('uuid');
             $table->softdeletes();
             $table->timestamps();
         });
+
+        DB::table('users')->insert(
+            array(
+                'usuario' => 'admin',
+                'nome' => 'administrador',
+                'cpf' => '000.000.000-00',
+                'email' => 'admin@teste.com',
+                'uuid' => Uuid::uuid4(),
+                'password' => Hash::make('123456')
+            )
+        );
+
     }
 
     /**
